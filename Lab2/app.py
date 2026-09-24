d6c209480467e3efbf30eb89d3dfad930210d08c
monman666 <echen.office@gmail.com>
2026-09-24 17:48:17 +0800

    Fix image navigation and update website title

--- Lab2/app.py
+++ Lab2/app.py
@@ -1,32 +1,47 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, redirect, url_for
 
 app = Flask(__name__)
 
 images = [
     "images/pic1.jpg",
     "images/pic2.jpg",
-    "images/pic3.jpg"
+    "images/pic3.jpg",
 ]
 
-current_index = 0  # 全域索引
+current_index = 0
+
 
 @app.route("/")
 def index():
-    global current_index
     img = images[current_index]
-    return render_template("index.html", image=img, idx=current_index, total=len(images))
+
+    return render_template(
+        "index.html",
+        image=img,
+        idx=current_index,
+        total=len(images),
+    )
+
 
 @app.route("/next")
 def next_img():
     global current_index
-    current_index += 1
+
+    # 最後一張再按下一張，會回到第一張
+    current_index = (current_index + 1) % len(images)
+
     return redirect(url_for("index"))
+
 
 @app.route("/prev")
 def prev_img():
     global current_index
-    current_index -= 1
+
+    # 第一張再按上一張，會跳到最後一張
+    current_index = (current_index - 1) % len(images)
+
     return redirect(url_for("index"))
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5050, debug=True)
